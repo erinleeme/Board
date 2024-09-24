630dbd8be0549d453f4e112cb0c83101e5651d98
erinleeme <gusdk1793@gmail.com>
2024-09-24 20:10:59 +0900

    feat:@NotBlank로 수정

--- src/main/java/com/example/board/dto/BoardRequestDto.java
+++ src/main/java/com/example/board/dto/BoardRequestDto.java
@@ -1,6 +1,6 @@
 package com.example.board.dto;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 
@@ -11,8 +11,8 @@ public class BoardRequestDto {
 
     private BoardRequestDto() {}
 
-    @NotNull(message = "제목을 작성해 주세요.")
+    @NotBlank(message = "제목을 작성해 주세요.")
     private String title;
-    @NotNull(message = "내용을 작성해 주세요.")
+    @NotBlank(message = "내용을 작성해 주세요.")
     private String content;
 }
